9b5b6af81d900f7df490ad8121965c00c8503c94
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-04-28 15:55:03 +0100

    Home route

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ function App() {
   return (
     <HashRouter>
       <nav>
-        <Link to="/">Bmi</Link>
+        <Link to="/Calculate-Anything">Bmi</Link>
         <Link to="/calculator">Simple Calculator</Link>
         <Link to="/percentage">Percentage</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Bmi />}/>
+        <Route path="/Calculate-Anything" element={<Bmi />}/>
         <Route path="/calculator" element={<Calculator />}/>
         <Route path="/percentage" element={<Percentage />}/>
       </Routes>
